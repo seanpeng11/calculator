8587b9f17ea11aa50cb4d060c450d46e3a5d2efe
Sean Peng <33929745+seanpeng11@users.noreply.github.com>
2022-12-24 23:24:02 +0800

    Add js operator functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function operate(oper, a, b) {
+    switch (oper) {
+        case "+": return add(a, b);
+        case "-": return subtract(a, b);
+        case "*": return multiply(a, b);
+        case "/": return divide(a, b);
+    }
+}

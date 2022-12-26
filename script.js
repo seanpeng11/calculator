e03c0943c06b6348f4a52caf7090b9659acd7526
Sean Peng <33929745+seanpeng11@users.noreply.github.com>
2022-12-26 10:25:08 +0800

    Add arithmetic logic to calculator

--- script.js
+++ script.js
@@ -21,9 +21,45 @@ function operate(oper, a, b) {
 
 const calcDisplay = document.querySelector(".calculator-display");
 const digits = document.querySelectorAll(".digit");
-const currentNumber = document.querySelector(".currentNumber");
+const currentNumberDisplayed = document.querySelector(".currentNumber");
+let currentNumber = "";
 digits.forEach(function(digit) {
     digit.addEventListener("click", function() {
-        currentNumber.textContent += this.textContent;
+        currentNumber += this.textContent
+        currentNumberDisplayed.textContent = currentNumber;
     });
+});
+
+const clear = document.getElementById("clear");
+const del = document.getElementById("del");
+clear.addEventListener("click", () => {
+    currentNumber = "";
+    currentNumberDisplayed.textContent = "";
+});
+del.addEventListener("click", () => {
+    currentNumber = currentNumber.slice(0, -1);
+    currentNumberDisplayed.textContent = currentNumber;
+});
+
+const operators = document.querySelectorAll(".operator");
+let currentOperator;
+let storedNumber = 0;
+operators.forEach(function(operator) {
+    operator.addEventListener("click", function() {
+        if (currentOperator !== undefined) {
+            currentNumber = operate(currentOperator, parseInt(storedNumber), parseInt(currentNumber));
+            currentNumberDisplayed.textContent = currentNumber;
+        }
+        currentOperator = operator.textContent;
+        storedNumber = currentNumber;
+        currentNumber = "";
+    });
+});
+
+const equals = document.getElementById("equals");
+equals.addEventListener("click", function() {
+    if (currentOperator === undefined) return;
+    currentNumber = operate(currentOperator, parseInt(storedNumber), parseInt(currentNumber));
+    currentNumberDisplayed.textContent = currentNumber;
+    currentOperator = undefined;
 });
